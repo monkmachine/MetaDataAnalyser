6371955f2ed18c1d126425955d3196f53af1026b
Dan Coldrick <monkmachine@gmail.com>
2023-01-06 20:05:49 +0000

    Data Tab populating

--- src/main/java/org/dsc/dbProviders/postGrestest.java
+++ src/main/java/org/dsc/dbProviders/postGrestest.java
@@ -27,7 +27,7 @@ public class postGrestest {
     public ResultSet runStatement () throws SQLException {
         Statement  st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-        ResultSet rs = st.executeQuery("select cast(value as Integer),text from (SELECT count(\"MetaDataKey\") as value,\"MetaDataKey\" as text FROM \"MetaData\".\"MetaData\" where \"Active\" IS null group by \"MetaDataKey\") t1 order by value desc limit 50");
+        ResultSet rs = st.executeQuery("select cast(value as Integer),text from (SELECT count(\"MetaDataKey\") as value,\"MetaDataKey\" as text FROM \"MetaData\".\"MetaData\" where \"Active\" IS null group by \"MetaDataKey\") t1 order by value desc limit 500");
 
         return rs;
     }
